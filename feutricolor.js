47a4321b9e1132c667dcb1333f6f2d5762023360
Farid Mhamdi <mhamdi.farid23@gmail.com>
2019-12-03 09:14:21 +0100

    tanaka

--- feutricolor.js
+++ feutricolor.js
@@ -1,17 +1,43 @@
 
-let idInterval1, idInterval2;
+let idInterval1, idInterval2 , idIntervalTri1, idIntervalTri2, idIntervalTri3;
+let quequunappui = false;
+
 
 function classic() {
-  idInterval1 = setInterval(green, 4000);
-  idInterval2 = setInterval(red, 6000);
+  idInterval1 = setInterval(red, 6000);
+  idInterval2 = setInterval(green, 10000);
+}
+
+function classic2() {
+  idIntervalTri1 = setInterval(greentri, 6000);
+  idIntervalTri2 = setInterval(orangetri, 9000);
+  idIntervalTri3 = setInterval(redtri, 10000);
 }
 
 function appel() {
-  clearInterval(idInterval1);
-  clearInterval(idInterval2);
-  setTimeout(green, 1000);
-  setTimeout(red, 5000);
-  setTimeout(classic, 6000);
+  if (document.getElementById("tricolor").className === "green" && !quequunappui){
+    quequunappui = true;
+    clearInterval(idInterval1);
+    clearInterval(idInterval2);
+    setTimeout(green, 1000);
+    setTimeout(red, 5000);
+    setTimeout(classic, 6000);
+    quequunappui = false;
+    }
+}
+
+function appel2() {
+  if (document.getElementById("tricolor").className === "green" && !quequunappui){
+    quequunappui = true;
+    clearInterval(idIntervalTri1);
+    clearInterval(idIntervalTri2);
+    clearInterval(idIntervalTri3);
+    setTimeout(orangetri, 0001);
+    setTimeout(redtri,1000);
+    setTimeout(greentri, 5000);
+    setTimeout(classic2, 6000);
+    quequunappui = false;
+    }
 }
 
 function green(){
@@ -19,11 +45,27 @@ function green(){
   element.classList.replace("red","green");
 }
 
-
 function red(){
   let element = document.getElementById("red");
   element.classList.replace("green","red");
 }
 
+function greentri(){
+  let element = document.getElementById("tricolor");
+  element.classList.replace("red","green");
+}
 
+function orangetri(){
+  let element = document.getElementById("tricolor");
+  element.classList.replace("green","orange");
+}
 
+function redtri(){
+  let element = document.getElementById("tricolor");
+  element.classList.replace("orange","red");
+}
+
+/*function changerCouleur(balise, source, cible){
+  let element = document.getElementById(balise);
+  element.classList.replace(source,cible);
+}*/
